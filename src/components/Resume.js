e63e28178313646c3294f3100fbe84ddcd0b74d3
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-01-11 16:15:35 -0500

    first push

---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Resume() {
+    return (
+        <section id="resume">
+            <div className="container px-5 py-10 mx-auto">
+                <div className="text-center mb-20">
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+                        Resume
+                    </h1>
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "PixaRadar",
+    subtitle: "MERN Stack",
+    description:
+      "This is my first group project. We created an app that can select movie titles based on your mood you select.",
+    image: "./pixaradar.jpg",
+    link: "https://github.com/askarrizvi/project1",
+  },
+  {
+    title: "Run Buddy",
+    subtitle: "HTML and CSS",
+    description:
+      "First page i made myself using html and css and deployed through github pages.",
+    image: "./Run-buddy.jpg",
+    link: "https://scottskinn.github.io/run-buddy",
+  },
+  {
+    title: "Developers Tech Blog",
+    subtitle: "JavaScript",
+    description:
+      "A blog page made with JS where a user can post comments and reply.",
+    image: "./Developed-tech-blog.jpg",
+    link: "https://shrouded-escarpment-46174.herokuapp.com/login",
+  },
+  {
+    title: "Photo Port",
+    subtitle: "React",
+    description:
+      "A photo gallery made with React as a single page application project.",
+    image: "./Photo-port.jpg",
+    link: "https://scottskinn.github.io/photo-port/",
+  },
+];
+
+export const resume = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Node",
+  "React",
+];
