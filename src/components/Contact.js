fa5c07688fa56d5321b6fed9488881ccfd1af93f
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-01-17 13:08:44 -0500

    contact submit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,8 +29,9 @@ export default function Contact() {
   return (
     <section id="contact" className="relative">
         <form
-          netlify netlify-honeypot
+          netlify netlify-honeypot 
           name="contact"
+          onSubmit={handleSubmit}
           className="flex flex-col items-center w-full md:py-8 mt-8 md:mt-0">
           <input type="hidden" name="contact" value="contact" />
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
@@ -48,6 +49,7 @@ export default function Contact() {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative w-9/12 mb-4">
@@ -59,6 +61,7 @@ export default function Contact() {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative w-9/12 mb-4">
@@ -71,6 +74,7 @@ export default function Contact() {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
